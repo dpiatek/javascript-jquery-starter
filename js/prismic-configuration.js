5d1481e74e196c99a4d0a13a206d2e45ec59ed5d
Dominik Piatek <do.piatek@gmail.com>
2013-10-23 20:16:15 +0100

    Point to teatr-kleks repo

--- js/prismic-configuration.js
+++ js/prismic-configuration.js
@@ -1,14 +1,14 @@
 var Configuration = {
 
   // -- API endpoint
-  apiEndpoint: 'https://lesbonneschoses.prismic.io/api',
+  apiEndpoint: 'https://teatr-kleks.prismic.io/api',
 
   // -- Access token if the Master is not open
   // accessToken: 'xxxxxx',
 
   // -- OAuth
-  // clientId: 'xxxxxx',
-  // clientSecret: 'xxxxxx',
+  clientId: 'UmgawrO534ge8mjF',
+  clientSecret: '8b8fe95ff8b202d346ef71945fe1f610',
 
   // -- Links resolution rules
   linkResolver: function(ctx, doc) {
